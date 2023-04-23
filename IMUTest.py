df1c145c8cb0e9b9b5ab369bc6e98203a7124fe2
Planesman22 <shawnsql@gmail.com>
2023-04-23 00:02:14 -0400

    More Sensor setup and test

--- IMUTest.py
+++ IMUTest.py
@@ -7,7 +7,7 @@ submodules = dir(adafruit_lsm6ds)
 print(submodules)
 
 I2C = board.I2C()  # uses board.SCL and board.SDA
-Sensor = adafruit_lsm6ds.LSM6DS(I2C)
+Sensor = adafruit_lsm6ds.lsm6dsox(I2C)
 
 while True:
     os.system('clear')
