a3331a11b4da54bca0450483395f82d766054b74
Planesman22 <shawnsql@gmail.com>
2023-04-23 00:57:16 -0400

    More Sensor setup and test

--- DataCollect.py
+++ DataCollect.py
@@ -1,32 +1,26 @@
 import os
-import board
 import time
+import board
+import busio
+import pwmio
 import shawnc
-import RPi.GPIO as GPIO
-# from adafruit_lsm6ds.ism330dhcx import ISM330DHCX
+from adafruit_lsm6ds.ism330dhcx import ISM330DHCX
 from time import sleep
 
-# GPIO Init
-PWMPin = 12
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PWMPin, GPIO.OUT)
-ESC = GPIO.PWM(PWMPin, 400)
-ESC.start(0)
-
-ESC.ChangeDutyCycle(shawnc.PowerToDC(0.0))
-
-'''
-# Sensor Init
-I2C = board.I2C()  # uses board.SCL and board.SDA
+# I2C Acc Init
+I2C = busio.I2C(board.SCL, board.SDA)  # uses board.SCL and board.SDA
 Sensor = ISM330DHCX(I2C)
 
+# PWM ESC Init
+ESC = pwmio.PWMOut(board.D18, frequency=400)
+
+ESC.duty_cycle(shawnc.PowerToDC(0.0))
+
 # Change power to 0 so we don't kill ourselves
-ESC.ChangeDutyCycle(shawnc.PowerToDC(0.0))
+ESC.duty_cycle(shawnc.PowerToDC(0.0))
 
 # Literally wait a second, for vibe check
 sleep(1)
 
-ESC.ChangeDutyCycle(shawnc.PowerToDC(0.3))
+ESC.duty_cycle(shawnc.PowerToDC(0.3))
 sleep(10)
-'''
--- ESCCalibrate.py
+++ ESCCalibrate.py
@@ -1,23 +1,18 @@
-import RPi.GPIO as GPIO
+import board
+import pwmio
 import shawnc
 from time import sleep
 
-#GPIO Setup
-PWMPin = 12
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PWMPin, GPIO.OUT)
-
-ESC = GPIO.PWM(PWMPin, 400)
-ESC.start(0)
+# PWM ESC Init
+ESC = pwmio.PWMOut(board.D18, frequency=400)
 
 # ESC Calibration
-ESC.ChangeDutyCycle(shawnc.PWToDC(2000))
+ESC.duty_cycle(shawnc.PWToDC(2000))
 while True:
     UserInput = input("Please Connect Battery! (Enter Y to continue)")
     if UserInput.lower() == 'y':
         break
 sleep(3)
-ESC.ChangeDutyCycle(shawnc.PWToDC(1000))
+ESC.duty_cycle(shawnc.PWToDC(1000))
 sleep(15)
 print("ESC should be calibrated, please check tones!")
