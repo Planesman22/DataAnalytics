7cfb4229080087e0bd98934bea3761b1f74a33b8
Planesman22 <shawnsql@gmail.com>
2023-04-22 23:54:03 -0400

    More Sensor setup and test

--- IMUTest.py
+++ IMUTest.py
@@ -1,11 +1,10 @@
 import os
 import time
 import board
-import busio
-import adafruit_lsm6ds
+from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 
-I2C = busio.I2C(board.SCL, board.SDA)
-Sensor = adafruit_lsm6ds.LSM6DSOX(I2C)
+I2C = board.I2C()  # uses board.SCL and board.SDA
+Sensor = LSM6DSOX(I2C)
 
 while True:
     os.system('clear')
