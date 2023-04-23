f5075bff0cda5290c9fe89e5d68d651e1ecb2dd2
Planesman22 <shawnsql@gmail.com>
2023-04-23 01:05:32 -0400

    More Sensor setup and test

--- DataCollect.py
+++ DataCollect.py
@@ -14,13 +14,17 @@ Sensor = ISM330DHCX(I2C)
 # PWM ESC Init
 ESC = pwmio.PWMOut(board.D18, frequency=400)
 
-ESC.duty_cycle = shawnc.PowerToDC(0.0)
-
 # Change power to 0 so we don't kill ourselves
 ESC.duty_cycle = shawnc.PowerToDC(0.0)
 
 # Literally wait a second, for vibe check
 sleep(1)
 
-ESC.duty_cycle = shawnc.PowerToDC(0.3)
-sleep(10)
+ESC.duty_cycle = shawnc.PowerToDC(0.05)
+sleep(0.5)
+ESC.duty_cycle = shawnc.PowerToDC(0.10)
+sleep(0.5)
+ESC.duty_cycle = shawnc.PowerToDC(0.15)
+sleep(0.5)
+ESC.duty_cycle = shawnc.PowerToDC(0.20)
+sleep(0.5)
