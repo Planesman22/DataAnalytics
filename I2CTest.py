eb356afe658375a24f434a59719549c7d3ec02f2
Planesman22 <shawnsql@gmail.com>
2023-04-22 22:08:26 -0400

    Sensor Test

---
+++ I2CTest.py
@@ -0,0 +1,17 @@
+import smbus
+import os
+import time
+
+bus = smbus.SMBus(1)
+
+Address = 0x6a
+
+while True:
+    os.system('clear')
+
+    Data = bus.read_byte(address)
+
+    print(Data)
+
+    time.sleep(0.5)
+
